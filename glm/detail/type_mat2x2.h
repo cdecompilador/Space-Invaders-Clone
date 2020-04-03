4e5cc0f08112fd1808626f132a85e6e808514679
ElGusanitoPLAY <nyagouno@gmail.com>
2020-04-03 18:23:13 +0200

    deleting shitty libs

--- glm/detail/type_mat2x2.h
+++
@@ -1,6 +0,0 @@
-#ifndef GLM_DETAIL_TYPE_MAT2X2_H
-#define GLM_DETAIL_TYPE_MAT2X2_H
-
-
-
-#endif /* GLM_DETAIL_TYPE_MAT2X2_H */
--- glm/glm.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLM_H
-#define GLM_H
-
-#include "vec1.h"
-#include "vec2.h"
-#include "vec3.h"
-#include "vec4.h"
-
-#endif /* GLM_H */
